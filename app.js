f3279c6d711abde533a6fcec81a80f8e44b00187
Abdallah Bakr <abab231196@gmail.com>
2020-08-05 01:03:58 +0200

    final project

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require( __dirname + '/date.js');
 
 const app = express();
+app.listen(3000, () => console.log('Server started at port 3000.'));
 
-app.listen(4040, () => console.log('Server started at port 4040.'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.send("Hi, every one"))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'))
+
+let items = ['learning EJS', 'Studying React JS'];
+let itemsWork = [];
+
+app.get('/', (req, res) => {
+	
+	let day = date.getDate();
+
+	res.render('list', {
+		listName: day,
+		newItems: items
+	});
+})
+
+app.post('/', (req, res) => {
+	let item = req.body.newItem;
+	console.log(req.body.button);
+	
+	if (req.body.button === 'Work') {
+		item === '' ? itemsWork : itemsWork.push(item);	
+		res.redirect('/work');
+	} else {
+		item === '' ? items : items.push(item);
+		res.redirect('/');
+	}
+})
+
+app.get('/work', (req, res) => {
+	res.render('list', {
+		listName: 'Work List',
+		newItems: itemsWork
+	})
+})
+
+app.get('/about', (req, res) => {
+	res.render('about')
+})
---
+++ date.js
@@ -0,0 +1,10 @@
+exports.getDate = () => {
+	let options = {
+		weekday: 'long',
+		month: 'long',
+		day: 'numeric'
+	}
+
+	let today = new Date();
+	return today.toLocaleDateString('en-US', options);
+}
